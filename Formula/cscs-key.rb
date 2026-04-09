4417ff1f298343236957bed702818b7117113d81
Matteo Seclì <secli.matteo@gmail.com>
2026-04-09 15:15:14 +0200

    Add shell completion for cscs-key - Shell completion instructions in caveats - Small fixes to pass brew audit - Head repo for --head installs

--- Formula/cscs-key.rb
+++ Formula/cscs-key.rb
@@ -1,19 +1,47 @@
 class CscsKey < Formula
   desc "CLI tool to manage SSH keys for the Swiss National Supercomputing Centre (CSCS)"
-  homepage "https://github.com/eth-cscs/cscs-key"
-  version "1.0.0"
-  url "https://github.com/eth-cscs/cscs-key/archive/refs/tags/#{version}.tar.gz"
+  homepage "https://github.com/eth-cscs/cscs-key/blob/master/README.md"
+  url "https://github.com/eth-cscs/cscs-key/archive/refs/tags/1.0.0.tar.gz"
   sha256 "d31d72e6355b0e98fc2a6376b188e6ae91d0988c0d292e6d9a13fe8c324d6391"
+  head "https://github.com/eth-cscs/cscs-key.git", branch: "master"
 
   depends_on "rust" => :build
 
   on_linux do
-    depends_on "openssl@3"
     depends_on "pkgconf" => :build
+    depends_on "openssl@3"
   end
 
   def install
     system "cargo", "install", *std_cargo_args(path: ".")
+  end
+
+  def caveats
+    shell = Utils::Shell.preferred || Utils::Shell.parent
+    profile = Utils::Shell.profile
+
+    command = case shell
+    when :bash, :zsh
+      "echo 'source <(cscs-key completion #{shell})' >> #{profile}"
+    when :fish
+      "echo 'cscs-key completion fish | source' >> #{profile}"
+    when :pwsh
+      "Add-Content -Path #{profile} -Value 'cscs-key completion powershell | Out-String | Invoke-Expression'"
+    end
+
+    if command
+      <<~EOS
+        To enable shell completion for your current shell, run:
+          #{command}
+      EOS
+    else
+      <<~EOS
+        Shell completion is available via:
+          cscs-key completion <shell>
+
+        Supported shells: bash, zsh, fish, powershell, elvish.
+      EOS
+    end
   end
 
   test do
